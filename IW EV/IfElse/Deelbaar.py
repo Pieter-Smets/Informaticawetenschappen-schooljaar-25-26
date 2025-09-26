4e12b4358604c5c77411eb2899860b7930ec5dd6
Pieter-Smets <151025768+Pieter-Smets@users.noreply.github.com>
2025-09-26 09:40:48 +0200

    aanmaak map If Else EV

---
+++ IW EV/IfElse/Deelbaar.py
@@ -0,0 +1,7 @@
+getal = int(input("Geef een getal: "))
+getal2 = 9
+
+if getal%getal2 == 0:
+    print(getal, " is deelbaar door ", getal2)
+else:
+    print(getal, " is niet deelbaar door ", getal2)
---
+++ IW EV/IfElse/Voorbeeld.py
@@ -0,0 +1,8 @@
+test = input("geef een letter in: ")
+
+if test == "links":
+    print("De controle is geslaagd")
+elif controle == 10:
+    print("De controle is niet geslaagd")
+else:
+    print("De controle is mislukt")
